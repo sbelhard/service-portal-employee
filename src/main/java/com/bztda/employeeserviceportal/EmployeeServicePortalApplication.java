5b4f12ab77c503d4dce669222b350724cd73a6b9
bigory <igorboldysh@gmail.com>
2020-12-11 17:38:43 +0300

    add default Spring Boot Application

---
+++ src/main/java/com/bztda/employeeserviceportal/EmployeeServicePortalApplication.java
@@ -0,0 +1,13 @@
+package com.bztda.employeeserviceportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class EmployeeServicePortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeServicePortalApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bztda/employeeserviceportal/EmployeeServicePortalApplicationTests.java
@@ -0,0 +1,14 @@
+package com.bztda.employeeserviceportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class EmployeeServicePortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
