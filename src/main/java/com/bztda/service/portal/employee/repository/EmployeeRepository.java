d0ef4c568565ce6214a20e730d7c3fd81856c990
bigory <igorboldysh@gmail.com>
2020-12-23 19:39:32 +0300

    add data database

--- src/main/java/com/bztda/service/portal/employee/repository/EmployeeRepository.java
+++ src/main/java/com/bztda/service/portal/employee/repository/EmployeeRepository.java
@@ -1,11 +1,15 @@
 package com.bztda.service.portal.employee.repository;
 
 import com.bztda.service.portal.employee.entity.Employee;
+import com.bztda.service.portal.employee.entity.Evaluation;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
+
 
 }
