6febe90ca9b70160f38420f1fb3bd94ea814589d
bigory <igorboldysh@gmail.com>
2020-12-24 15:06:19 +0300

    fix Department

--- src/main/java/com/bztda/service/portal/employee/entity/Department.java
+++ src/main/java/com/bztda/service/portal/employee/entity/Department.java
@@ -1,5 +1,6 @@
 package com.bztda.service.portal.employee.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -31,7 +32,8 @@ public class Department extends BaseEntity<Long> {
 
 	@Column(name = "department")
 	private String department;
-
+	
+	@JsonIgnore
 	@OneToMany(mappedBy = "department")
 	private List<Employee> employees = new ArrayList<>();
 }
