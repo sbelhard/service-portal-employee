1785b25eb16bfec118382ac919aa01107edd480b
bigory <igorboldysh@gmail.com>
2021-01-06 23:09:22 +0300

    fix cors

--- src/main/java/com/bztda/service/portal/employee/security/SecurityConfig.java
+++ src/main/java/com/bztda/service/portal/employee/security/SecurityConfig.java
@@ -34,7 +34,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http
+		http	.csrf()
+				.disable()
 				.cors()
 				.and()
 				.authorizeRequests()
