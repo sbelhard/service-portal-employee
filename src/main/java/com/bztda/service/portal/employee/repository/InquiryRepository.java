0f5dc6b97c96c3415ba5983b4b7bba6b84ccf9ae
bigory <igorboldysh@gmail.com>
2021-02-02 20:40:57 +0300

    add inquiry

--- src/main/java/com/bztda/service/portal/employee/repository/InquiryRepository.java
+++ src/main/java/com/bztda/service/portal/employee/repository/InquiryRepository.java
@@ -2,11 +2,8 @@ package com.bztda.service.portal.employee.repository;
 
 import com.bztda.service.portal.employee.entity.Employee;
 import com.bztda.service.portal.employee.entity.Inquiry;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,10 +12,5 @@ import java.util.List;
 public interface InquiryRepository extends PagingAndSortingRepository<Inquiry, Long> {
 
 	List<Inquiry> findAllByEmployee(Employee employee);
-
-	@Modifying
-	@Transactional
-	@Query("update DataEmployee1C d set  d.isEmployee=?1 where d.numberPass=?2")
-	void update(boolean isEmployee, String numberPass);
 
 }
